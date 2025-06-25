af5552428d8e356916a262092be30add0f3e7bb8
Xinyan Lu <lvxinyan1992@gmail.com>
2025-06-26 00:06:31 +0800

    Initial Commit

---
+++ NetworkSwitch/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  NetworkSwitch
+//
+//  Created by 吕心言 on 26/6/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ NetworkSwitchTests/NetworkSwitchTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSwitchTests.swift
+//  NetworkSwitchTests
+//
+//  Created by 吕心言 on 26/6/2025.
+//
+
+import Testing
+@testable import NetworkSwitch
+
+struct NetworkSwitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
